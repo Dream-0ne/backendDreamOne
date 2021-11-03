4971a13da6bec033d4eac52f522a2e3086291503
unknown <fardeenfmeeran@gmail.com>
2021-11-02 21:35:43 -0500

    postgres please work + f string

--- mySqlDB.py
+++ mySqlDB.py
@@ -22,9 +22,8 @@ def createOccasions():
   occasions = ["Birthday","Nightout","Party","Date","Exploring"]
   cursor.execute(f"CREATE table if not exists occasions ( name text)")
   for i in range(5):
-    sql = "INSERT INTO occasions (name) VALUES (%s)"
-    val = (str(occasions[i]))
-    cursor.execute(sql,val)
+    sql = f"INSERT INTO occasions (name) VALUES {occasions[i]})"
+    cursor.execute(sql)
   connection.commit()
 def createOccasionsFilters():
   cursor.execute(f"CREATE table if not exists occasionsfilters ( occasionid integer, filter text)")
