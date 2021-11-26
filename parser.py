bc79cbaf809d5ca2f44118e5e37aebec2255ea75
Meerxn <fardeenfmeeran@gmail.com>
2021-11-26 11:52:30 -0600

    Added more fields

--- parser.py
+++ parser.py
@@ -10,7 +10,15 @@ data = []
 businessData = []
 print("new run")
 mainDict = [{"Fitler" : "Food" , "Tags": ["Mexican","Indian","Chinese","Vegan","Arabic"], "Query": ["Mexican Food", "Indian Food", "Chinese Food", "Vegan Food",'Arabic Food']}, 
-{"Fitler" : "Shopping" , "Tags": ["Shoes","Clothes","Home Decor","Grocery","Electronics","Woodwork"], "Query": ["Shoes Store", "Clothes Store", "Home Decor Store" , "Grocery Store","Electronics Store", "Woodwork store"]}]
+{"Fitler" : "Shopping" , "Tags": ["Shoes","Clothes","Home Decor","Grocery","Electronics","Woodwork"], "Query": ["Shoes Store", "Clothes Store", "Home Decor Store" , "Grocery Store","Electronics Store", "Woodwork store"]},
+{"Fitler" : "Gaming" , "Tags": ["Theme Park","Arcade","Gaming Stores","Go karts","Trampoline Park","Horseback Riding", "Gaming Bars"], "Query": ["Theme Park","Arcade","Gaming Stores","Go karts","Trampoline Park","Horseback Riding", "Gaming Bars"]},
+{"Fitler" : "Travel" , "Tags": ["Tourist Visa", "Car rental", "Motel"], "Query": ["Tourist Visa", "Car rental", "Motel"]},
+{"Fitler" : "Arts and Craft" , "Tags": ["Pottery", "Stationary", "Art Studio", "Face Painting"], "Query": ["Pottery Store", "Stationary Store", "Art Studio", "Face Painting"]},
+{"Filter": "Theatres", "Tags": ["Musical", "Movies"], "Query":["Musical", "Movies"]},
+{"Fitler": "Tours", "Tags": ["Tours"], "Query":["Local Guided Tours"]},
+{"Fitler": "Sightseeing", "Tags": ["Local"], "Query":["Local Attractions"]}]
+
+
 print(mainDict)
 
 for category in mainDict:
@@ -34,7 +42,7 @@ for category in mainDict:
                 continue
         tag_entry["bus"] = currList
         businessData.append(tag_entry)
-print(businessData[0]['bus'][0])
+print(businessData[10]['bus'][0])
  
        
 
